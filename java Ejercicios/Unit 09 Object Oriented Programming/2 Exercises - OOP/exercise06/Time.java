a352720d78e11e9b4b034ef1620e06bc3ecf43d0
ruben-kush <ruben.mendoza@uaem.mx>
2026-08-29 08:47:50 -0600

    Add files via upload

---
+++ java Ejercicios/Unit 09 Object Oriented Programming/2 Exercises - OOP/exercise06/Time.java
@@ -0,0 +1,67 @@
+public class Time {
+
+  private int hours;
+  private int minutes;
+  private int seconds;
+
+  public Time(int hours, int minutes, int seconds) {
+    this.hours = hours;
+    this.minutes = minutes;
+    this.seconds = seconds;
+  }
+
+
+  public Time sum(Time interval) {
+    Time newTime = new Time(0, 0, 0);
+
+    newTime.seconds = this.seconds + interval.seconds;
+    newTime.minutes = this.minutes + interval.minutes;
+    newTime.hours = this.hours + interval.hours;
+    if (newTime.seconds >= 60) {
+      newTime.minutes++;
+      newTime.seconds = newTime.seconds - 60;
+    }
+
+    if (newTime.minutes >= 60) {
+      newTime.hours++;
+      newTime.minutes = newTime.minutes - 60;
+
+    }
+
+    return newTime;
+  }
+
+  public Time subtract(Time interval) {
+    Time newTime = new Time(0, 0, 0);
+
+    newTime.seconds = this.seconds - interval.seconds;
+    newTime.minutes = this.minutes - interval.minutes;
+    newTime.hours = this.hours - interval.hours;
+    if (newTime.seconds < 0) {
+      newTime.minutes--;
+      newTime.seconds = newTime.seconds + 60;
+    }
+
+    if (newTime.minutes < 0) {
+      newTime.hours--;
+      newTime.minutes = newTime.minutes + 60;
+
+    }
+
+    return newTime;
+  }
+
+
+  @Override
+  public String toString() {
+    if (this.hours < 0) {
+      return "Negative time";
+    } else {
+
+      return this.hours + "h" + " " + this.minutes + "m" + " " + this.seconds + "s";
+    }
+
+  }
+}
+
+
---
+++ java Ejercicios/Unit 09 Object Oriented Programming/2 Exercises - OOP/exercise12/Libro.java
@@ -0,0 +1,38 @@
+public class Libro extends Publicacion implements Prestable{
+  private boolean prestado;
+
+  public Libro(String isbn, String titulo, int agnoPubli) {
+    super(isbn, titulo, agnoPubli);
+    this.prestado = false;
+  }
+
+  @Override
+  public void presta() {
+    if(this.prestado) {
+      System.out.println("Lo siento, ese libro ya esta prestado");
+    } else {
+      this.prestado = true;
+    }
+    
+  }
+
+  @Override
+  public void devuelve() {
+    this.prestado = false;
+  }
+
+  @Override
+  public boolean estaPrestado() {
+    return this.prestado;
+  }
+
+  @Override
+  public String toString() {
+    if (this.prestado) {
+      return super.toString() + " (" + "prestado)";
+    } else {
+      return super.toString() + " (" + "no prestado)";
+    }
+
+  }
+}
