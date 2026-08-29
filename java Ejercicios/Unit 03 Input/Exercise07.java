66f2576c0ccdbdf8250aa4105e10356c0ff195f2
ruben-kush <ruben.mendoza@uaem.mx>
2026-08-29 08:41:27 -0600

    Add files via upload

---
+++ java Ejercicios/Unit 03 Input/Exercise07.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class Exercise07 {
+
+  public static void main (String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.println("Please enter the tax base of the product");
+    double taxBase = sc.nextDouble();
+    double totalPrice = taxBase + taxBase * 21 / 100;
+   
+    System.out.println("Tax base: " + taxBase + " euros");
+    System.out.println("Total price: " + Math.round(totalPrice * 100) / 100D + " euros");
+  }
+}
+
